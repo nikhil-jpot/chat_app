670c18545e66495654358bd6372b4727a87ca57a
nikhil-jpot <nikhilchoudhary770@gmail.com>
2025-02-19 21:55:37 +0530

    set up cloudinary and create updateProfile, check api

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -95,3 +95,32 @@ export const logout=(req,res)=>{
         res.status(500).json({message:"Internal Server Error"});
     }
 }
+
+export const updateProfile=async(req,res)=>{
+    try {
+        const {profilePic}=req.body;
+        const userId=req.user._id;
+
+        if(!profilePic){
+            return res.status(400).json({message:"Profile Pic is required"});
+        }
+
+        const uploadResponse=await cloudinary.uploader.upload(profilePic);
+        const updatedUser=User.findByIdAndUpdate(userId,{profilePic:uploadResponse.secure_url},{new:true});
+        res.status(200).json(updatedUser);
+
+    } catch (error) {
+        console.log("Error in profilePic Update",error.message);
+        res.status(500).json({message:"Internal Server Error"});
+    }
+}
+
+export const checkAuth=(req,res)=>{
+    try {
+        res.status(200).json(req.user);
+    } catch (error) {
+        console.log("Error in checkAuth route",error.message);
+        res.status(500).json({message:"Internal Server Error"});
+        
+    }
+}
